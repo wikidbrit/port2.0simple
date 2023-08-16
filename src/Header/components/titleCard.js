351b7cd574e0ae23516ecd680a1a9ad15d1fe0c5
wikidbrit <pflemingonline@gmail.com>
2023-08-16 14:33:59 +0200

    add offset for designer text to highlight

--- src/Header/components/titleCard.js
+++ src/Header/components/titleCard.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const TitleCard = () => {
   return (
-    <div className="text-gray-300">
+    <div className="text-gray-300 space-y-5">
       <p>Hey, I'm Paul Fleming. Welcome to my portfolio page!</p>
-      <p>Developer & Designer</p>
+      <p className="flex flex-row text-6xl">
+        Developer &{' '}
+        <div className="relative poetsenOne">
+          <span className="absolute z-10 top-0.5 left-1 text-pink-500">
+            Designer
+          </span>
+          <span className="absolute left-2 z-20">Designer</span>
+        </div>
+      </p>
       <p>
         I help you ideate, iterate, and developer the best solution your brand
         deserves.
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({ children }) => {
   return (
     <div className="w-scree h-screen bg-zinc-900">
-      <div>{children}</div>
+      <div className="p-5">{children}</div>
     </div>
   );
 };
