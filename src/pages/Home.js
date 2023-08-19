23223794c806424739e85916275667bd1e0710b3
wikidbrit <pflemingonline@gmail.com>
2023-08-19 20:19:45 +0200

    make splash screen handler

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import Header from '../components/Header/Header';
 import Projects from '../Projects/Projects';
 import Contact from '../components/contact/Contact';
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
       <Layout>
-        <Header />
-        <Projects />
-        <Contact />
+        {loading && <div className="h-screen">Loading</div>}
+        {!loading && (
+          <div className="fadeIn">
+            <Header />
+            <Projects />
+            <Contact />
+          </div>
+        )}
       </Layout>
     </>
   );
