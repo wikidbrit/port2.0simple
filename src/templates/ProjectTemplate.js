e648da2f5bcfcab32b330b5c3a74d2469573e343
wikidbrit <pflemingonline@gmail.com>
2023-08-20 20:06:54 +0200

    weird log

--- src/templates/ProjectTemplate.js
+++ src/templates/ProjectTemplate.js
@@ -16,6 +16,7 @@ const ProjectTemplate = () => {
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
       >
         <div></div>
