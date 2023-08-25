d758dd40e4b64a58625054be8ae9f48e3e58fb60
wikidbrit <pflemingonline@gmail.com>
2023-08-25 14:10:19 +0200

    fix background image in place and ensure it renders behind content

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import ThemeContext from "../context/ThemeContext";
 import MainNavigation from "./navBar/MainNavigation";
 import { BGDarkmode, BGLightmode } from "../assets/svgs";
 
-const Layout = ({ children, handleChange }) => {
+const Layout = ({ children }) => {
   const theme = useContext(ThemeContext);
 
   return (
@@ -13,21 +13,29 @@ const Layout = ({ children, handleChange }) => {
         theme === "dark"
           ? "from-bgBlueDark to-bgBlueLight"
           : "from-steel-100 to-steel-300",
-        "h-100% relative left-0 top-0 z-50 min-h-screen w-screen bg-gradient-to-br",
+        "h-100% relative left-0 top-0 min-h-screen w-screen bg-gradient-to-br",
       )}
     >
       <MainNavigation />
+
+      <div
+        style={{ zIndex: 1 }} // Higher value than background image's z-index
+        className="relative mx-auto max-w-[1280px] p-10"
+      >
+        {children}
+      </div>
+
       <div
         className={classNames(
           theme === "dark" ? "opacity-30" : "opacity-100",
-          " absolute left-0 right-0 mx-auto w-full max-w-[1280px] p-20 transition-all duration-300 ease-in-out",
+          "pointer-events-none fixed left-0 right-0 top-10 mx-auto w-full max-w-[1280px] p-20 transition-all duration-300 ease-in-out",
         )}
+        style={{ zIndex: 0 }} // Lower value to keep the background behind
       >
-        {theme === "dark" && <BGDarkmode />}
-        {theme === "light" && <BGLightmode />}
+        {theme === "dark" ? <BGDarkmode /> : <BGLightmode />}
       </div>
-      <div className="mx-auto max-w-[1280px] p-10">{children}</div>
     </div>
   );
 };
+
 export default Layout;
--- src/templates/ProjectTemplate.js
+++ src/templates/ProjectTemplate.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 
 import useGetSlugContent from "../hooks/useGetSlugContent";
@@ -7,27 +7,12 @@ import { FiArrowLeft } from "react-icons/fi";
 import { motion } from "framer-motion";
 import NavButton from "../components/buttons/NavButton";
 import ThemeContext from "../context/ThemeContext";
-import classNames from "classnames";
 
 const ProjectTemplate = ({ handleChange }) => {
   const { slug } = useParams();
   const { data } = useGetSlugContent({ slug });
 
   const theme = useContext(ThemeContext);
-
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const handleScroll = () => {
-    const position = window.scrollY;
-    setScrollPosition(position);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   if (!data) {
     return (
@@ -44,19 +29,12 @@ const ProjectTemplate = ({ handleChange }) => {
 
   return (
     <motion.div
+      className=""
       exit={{ opacity: 0, scale: 0.95 }}
       initial={{ opacity: 0, scale: 0.9 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 1, ease: [0.87, 0, 0.13, 1] }}
     >
-      <div
-        className={classNames(
-          scrollPosition >= 200
-            ? "opacity-1 pointer-events-auto "
-            : "pointer-events-none opacity-0",
-          "fixed top-4 -translate-x-20 duration-300",
-        )}
-      ></div>
       <NavButton hash icon={<FiArrowLeft />} theme={theme} to={"/#Projects"} />
 
       <p className="text-[4.2rem] text-neoGrey ">{data[0].title}</p>
