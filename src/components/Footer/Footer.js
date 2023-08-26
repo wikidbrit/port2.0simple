050888e9f4c1f0a8da0c4e21768d6dd45de147f6
wikidbrit <pflemingonline@gmail.com>
2023-08-26 15:32:08 +0200

    fix footer light mode issues

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -51,7 +51,7 @@ const Footer = ({ handleChange }) => {
           <ul className="space-y-4">
             <li className="flex flex-row items-center space-x-2">
               <div className=" text-lightWater">
-                <TbMessage2Share color="#D6D6D6" size={"1.2rem"} />
+                <TbMessage2Share size={"1.2rem"} />
               </div>
 
               <a
@@ -64,13 +64,13 @@ const Footer = ({ handleChange }) => {
             </li>
             <li className="flex flex-row items-center space-x-2">
               <div className=" text-lightWater">
-                <TbPhone color="#D6D6D6" size={"1.2rem"} />
+                <TbPhone size={"1.2rem"} />
               </div>
               <p>073 953 94 69</p>
             </li>
             <li className="flex flex-row items-center space-x-2">
               <div className=" text-lightWater">
-                <TbMapPin color="#D6D6D6" size={"1.2rem"} />
+                <TbMapPin size={"1.2rem"} />
               </div>
               <p>Stockholm, Sweden</p>
             </li>
@@ -88,7 +88,11 @@ const Footer = ({ handleChange }) => {
           <Popup
             modal
             nested
-            trigger={<IconButtonSmall icon={<TbUserShare size={"1.2rem"} />} />}
+            trigger={
+              <IconButtonSmall
+                icon={<TbUserShare color="#D6D6D6" size={"1.2rem"} />}
+              />
+            }
             {...{ overlayStyle, contentStyle }}
           >
             {(close) => <AboutMeModal close={close} />}
@@ -97,7 +101,9 @@ const Footer = ({ handleChange }) => {
             modal
             nested
             trigger={
-              <IconButtonSmall icon={<TbMessageShare size={"1.2rem"} />} />
+              <IconButtonSmall
+                icon={<TbMessageShare color="#D6D6D6" size={"1.2rem"} />}
+              />
             }
             {...{ overlayStyle, contentStyle }}
           >
@@ -106,7 +112,11 @@ const Footer = ({ handleChange }) => {
           <Popup
             modal
             nested
-            trigger={<IconButtonSmall icon={<TbChecklist size={"1.2rem"} />} />}
+            trigger={
+              <IconButtonSmall
+                icon={<TbChecklist color="#D6D6D6" size={"1.2rem"} />}
+              />
+            }
             {...{ overlayStyle, contentStyle }}
           >
             {(close) => <SkillsModal close={close} />}
