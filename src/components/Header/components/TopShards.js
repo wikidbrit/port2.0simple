1e667d94446d0f80119d207765d622929636dacb
wikidbrit <pflemingonline@gmail.com>
2023-08-26 21:58:34 +0200

    remove delay from shards animation

--- src/components/Header/components/TopShards.js
+++ src/components/Header/components/TopShards.js
@@ -17,7 +17,7 @@ const TopShards = () => {
       <motion.div
         initial={{ y: 70, x: 150, scale: 0.1 }}
         animate={{ y: 0, x: -90, scale: 1.4 }}
-        transition={{ ease: "easeOut", duration: 2, delay: 0.2 }}
+        transition={{ ease: "easeOut", duration: 2 }}
         className="cursor-pointer"
         style={{ rotate: rotate1, willChange: "transform" }}
       >
@@ -32,7 +32,7 @@ const TopShards = () => {
       <motion.div
         initial={{ y: 60, x: 70, scale: 0.1 }}
         animate={{ y: -80, x: -60, scale: 4.4 }}
-        transition={{ ease: "easeOut", duration: 2, delay: 0.5 }}
+        transition={{ ease: "easeOut", duration: 2.3 }}
         className="w-6 cursor-pointer"
         style={{ rotate: rotate2, willChange: "transform" }}
       >
@@ -55,7 +55,7 @@ const TopShards = () => {
       <motion.div
         initial={{ y: 40, x: -20, scale: 0.2 }}
         animate={{ y: -30, x: 10, scale: 1.8 }}
-        transition={{ ease: "easeOut", duration: 2, delay: 0.8 }}
+        transition={{ ease: "easeOut", duration: 2.2 }}
         className="w-6 cursor-pointer"
         style={{ rotate: rotate1, willChange: "transform" }}
       >
@@ -70,7 +70,7 @@ const TopShards = () => {
       <motion.div
         initial={{ y: 60, x: -40, scale: 0.2 }}
         animate={{ y: 20, x: 60, scale: 1.8 }}
-        transition={{ ease: "easeOut", duration: 2, delay: 1 }}
+        transition={{ ease: "easeOut", duration: 1.8 }}
         className="w-6 cursor-pointer"
         style={{ rotate: rotate2, willChange: "transform" }}
       >
